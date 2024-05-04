976e00d5ef5951056c15991251b211eda9d1fdb9
Michael Chapman <chapman.michael.c@gmail.com>
2024-05-04 12:52:42 -0500

    Dynamic PWA Manifest generation
    This somewhat eliminates the PWA install issues on iOS after query params were introduced.
    Still want to revisit this.

---
+++ assets/dynamicPWA.js
@@ -0,0 +1,83 @@
+// Function to check if user agent is iOS
+function isiOS() {
+    return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+}
+
+// Function to check if user agent is Android
+function isAndroid() {
+    return /Android/i.test(navigator.userAgent);
+}
+
+function editStartUrlAndGenerateDataUrl(manifestUrl, newStartUrl) {
+    // Fetch the manifest.json file
+    return fetch(manifestUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Check if start_url exists in the manifest
+        if (data.start_url) {
+            originalStartUrl = data.start_url;
+            // Edit the start_url
+            data.start_url = newStartUrl;
+
+            // Convert the updated data back to JSON
+            const updatedManifest = JSON.stringify(data);
+
+            // Generate Data URL from updated JSON content
+            const dataUrl = 'data:application/manifest+json;charset=utf-8,' + encodeURIComponent(updatedManifest);
+            // Log the generated Data URL
+            //console.log('Generated Data URL:', dataUrl);
+            return dataUrl;
+        } else {
+            console.error('start_url not found in manifest.json');
+            throw new Error('start_url not found in manifest.json');
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching or updating manifest.json:', error);
+        return Promise.reject(error);
+    });
+}
+
+// Function to run the script
+function runScript(manifestUrl, newStartUrl) {
+    if (!manifestUrl) {
+        console.error('Manifest URL is required');
+        return;
+    }
+
+    if (isiOS()) {
+        // Perform iOS-specific action
+        //console.log("User is using iOS");
+        editStartUrlAndGenerateDataUrl(manifestUrl, newStartUrl)
+            .then(dataUrlOrStartUrl => {
+                document.querySelector("head > link").href = dataUrlOrStartUrl;
+//                //console.log('Generated Data URL:', dataUrlOrStartUrl);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    } else if (isAndroid()) {
+        // Perform Android-specific action
+        //console.log("User is using Android");
+        // Your Android specific code here
+    } else {
+        // Perform action for other user agents
+        //console.log("User is using some other platform");
+        // Your generic code here
+        editStartUrlAndGenerateDataUrl(manifestUrl, newStartUrl)
+            .then(dataUrlOrStartUrl => {
+                document.querySelector("head > link").href = dataUrlOrStartUrl;
+//                console.log('Generated Data URL:', dataUrlOrStartUrl);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+}
+
+runScript("./assets/manifest.json", "")
