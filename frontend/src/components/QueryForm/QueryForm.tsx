8c91c9272785f2039a28928c5681ee65d66b4a5b
Michael Chapman <chapman.michael.c@gmail.com>
2024-05-11 12:37:34 -0500

    Styling the Query Form and some cleanup

--- frontend/src/components/QueryForm/QueryForm.tsx
+++ frontend/src/components/QueryForm/QueryForm.tsx
@@ -1,3 +1,5 @@
+import { Button, Container, Flex, Group, Stack, TextInput } from "@mantine/core";
+import classes from "./QueryForm.module.css"
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
@@ -17,25 +19,50 @@ export function QueryForm({onFormSubmit  = (request: bookRequest) => {}}) {
         }
     }, [searchParams]);
 
+    function validateUrlInput() {
+        if(!link.startsWith("https://www.goodreads.com")) {
+            alert("URL must start with 'https://www.goodreads.com'")
+            return false
+        } else {
+            return true
+        }
+    }
+
     const handleFormSubmit = () => {
-        // Create a new URLSearchParams object
-        const params = new URLSearchParams(searchParams);
-    
-        // Set the "gr_id" parameter to the link value
-        params.set("gr_id", link);
-    
-        // Update the search parameters in the URL
-        setSearchParams(params);
-        
-        // Call the onFormSubmit function with the updated request
-        onFormSubmit({ num_books: 5, gr_url: link });
+
+        const validUrl = validateUrlInput()
+        if (validUrl) {
+            // Create a new URLSearchParams object
+            const params = new URLSearchParams(searchParams);
+
+            // Set the "gr_id" parameter to the link value
+            params.set("gr_id", link);
+
+            // Update the search parameters in the URL
+            setSearchParams(params);
+
+            // Call the onFormSubmit function with the updated request
+            onFormSubmit({ num_books: 5, gr_url: link });
+        }
       };
 
 
     return (
-        <>
-        <input type="text" value={link} onChange={(e) => setLink(e.target.value)}></input>
-        <button onClick={handleFormSubmit}>Get data</button>
-        </>
+        <Container size="md">
+            <Flex justify="Flex-start" align="Flex-end" gap="md">
+                <TextInput
+                    className={classes.input}
+                    label="Shelf Url"
+                    placeholder="Enter Goodreads Shelf URL"
+                    size="sm"
+                    value={link}
+                    // onChange={(e) => setLink(e.target.value)}
+                ></TextInput>
+            <Button 
+                className={classes.button}
+                onClick={handleFormSubmit}
+                >Get data</Button>
+            </Flex>
+        </Container>
     );
 }
