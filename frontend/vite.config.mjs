f8bd21b7e974b7dda18088177313a8ce2d4a1e16
Michael Chapman <chapman.michael.c@gmail.com>
2024-09-23 07:56:38 -0500

    More PWA Removal

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
