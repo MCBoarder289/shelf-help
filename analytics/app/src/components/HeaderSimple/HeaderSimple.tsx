5d32c8ed57bb8ec636d1a1f4ce6c98451da72e63
Michael Chapman <chapman.michael.c@gmail.com>
2024-10-01 19:25:24 -0500

    Fixing theme color for Analytics app

--- analytics/app/src/components/HeaderSimple/HeaderSimple.tsx
+++ analytics/app/src/components/HeaderSimple/HeaderSimple.tsx
@@ -2,12 +2,40 @@ import { Container, Title, rem, useMantineTheme, Switch, useMantineColorScheme }
 import classes from './HeaderSimple.module.css';
 import icon from "/images/icon.png"
 import { IconMoonStars, IconSun } from '@tabler/icons-react';
+import { useEffect } from 'react';
 
 
 export function HeaderSimple() {
  
   const {colorScheme, toggleColorScheme} = useMantineColorScheme();
 
+  // Function to dynamically update the theme color in the meta tag
+  const updateStatusBarColor = (theme: 'light' | 'dark') => {
+    const themeColor = theme === 'dark' ? '#242424' : '#ffffff';
+    const metaTag = document.querySelector('meta[name="theme-color"]');
+
+    if (metaTag) {
+      metaTag.setAttribute('content', themeColor);
+    }
+  };
+
+  function toggleColorSchemeAndTheme() {
+    toggleColorScheme()
+    const metaTag = document.querySelector('meta[name="theme-color"]');
+      if (metaTag) {
+        if (colorScheme === 'dark') {
+          metaTag.setAttribute('content', '#ffffff'); // Turn it back to white
+        } else {
+          metaTag.setAttribute('content', '#242424'); // Turn it back to dark
+        }
+      }
+    }
+
+      // On initial render, check the current theme and set the meta tag color accordingly
+  useEffect(() => {
+    const theme = colorScheme === 'dark' ? 'dark' : 'light';
+    updateStatusBarColor(theme);
+  }, []);
 
   const theme = useMantineTheme();
 
@@ -33,7 +61,7 @@ export function HeaderSimple() {
         <Title className={classes.title} style={{"--title-fw": "var(--mantine-h1-font-weight)", "--title-lh": "var(--mantine-h1-line-height)", "--title-fz": "var(--mantine-h2-font-size)"}}>
           Shelf Help Analytics
         </Title>
-        <Switch size="md" color="dark.4" onLabel={sunIcon} offLabel={moonIcon} checked={colorScheme === 'dark' ? true : false} onChange={toggleColorScheme} />
+        <Switch size="md" color="dark.4" onLabel={sunIcon} offLabel={moonIcon} checked={colorScheme === 'dark' ? true : false} onChange={toggleColorSchemeAndTheme} />
       </Container>
   );
 }
